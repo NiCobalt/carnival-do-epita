b54d775c72907fb3cfeeaec59b1e35c8cb608832
Nicolas Cendrier <nicolas.cendrier@epita.fr>
2019-09-25 20:29:33 +0200

    better

--- src/Game/PlayerIA/NicobaltPlayer.php
+++ src/Game/PlayerIA/NicobaltPlayer.php
@@ -2,7 +2,21 @@
 namespace Hackathon\PlayerIA;
 
 class NicobaltPlayer extends Player {
-    function getChoice() {
-        return 'paper';
+
+    protected $mySide;
+    protected $opponentSide;
+    protected $result;
+
+    public function getChoice() {
+        if ($this->result->getNbRound() != 0) {
+            if ($this->result->getLastChoiceFor($this->opponentSide) == 'paper')
+                return 'scissors';
+            elseif ($this->result->getLastChoiceFor($this->opponentSide) == 'rock')
+                return 'paper';
+            else
+                return 'rock';
+        }
+        else
+            return 'paper';
     }
 };
